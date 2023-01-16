6925ca82f37883537bb699e2c9347a0a3f4e6808
skshm <eminate894567@protonmail.com>
2023-01-16 16:55:55 +0530

    the sacred texts!

---
+++ Hello_World/hello.rs
@@ -0,0 +1,16 @@
+/*
+? Name: "First" Hello,
+? date: 16/01/2023,
+? Author: @midnight1938,
+*/
+
+// You can manipulate expressions more easily with block comments
+// than with line comments. Cuz you can use them in the middle of the code
+
+
+fn main() {
+    let name = "Skshm";
+
+    println!("Hello, world!");
+    println!("My name {}", name);
+}
