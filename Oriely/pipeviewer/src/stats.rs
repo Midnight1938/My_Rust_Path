3aea07d7c0a6ff1a0d77ea8216744efe32dff50d
skshm <eminate894567@protonmail.com>
2023-09-29 18:15:56 +0530

    created tiny submodule

--- Oriely/pipeviewer/src/stats.rs
+++ Oriely/pipeviewer/src/stats.rs
@@ -1,23 +1,37 @@
+mod timer;
+
 use crossbeam::channel::Receiver;
-use std::io::Result;
-use std::time::{Duration, Instant};
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent, Stylize},
+    terminal::{Clear, ClearType},
+};
+
+use std::time::Instant;
+use std::io::{self, Result, Stderr, Write};
+
+use timer::Timer;
 
 pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
+
     let start = Instant::now();
     let mut timer = Timer::new();
+
+    let mut stderr = io::stderr();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         timer.update();
         let rate_per_sec = (num_bytes as f64) / timer.delta.as_secs_f64();
         total_bytes += num_bytes;
-        if !silent && timer.ready{ // Outputs a second at a time
+        if !silent && timer.ready {
+            // Outputs a second at a time
             timer.ready = false;
-            eprint!(
-                "\r{} {} [{:.0}b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_sec
+                rate_per_sec,
             ); // :.0 removes fraction
         };
 
@@ -32,44 +46,28 @@ pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     Ok(())
 }
 
-trait TimeOutput{
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("{} ", bytes)).with(Color::Magenta);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!(" [{:.0}b/s]", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate),
+    );
+    let _ = stderr.flush();
+}
+
+trait TimeOutput {
     fn as_time(&self) -> String;
 }
-impl TimeOutput for u64{
-    fn as_time(&self) -> String{
-        let (hours, left) = (*self/3600, *self%3600);
-        let (minutes, seconds) = (left / 60, left%60);
+impl TimeOutput for u64 {
+    fn as_time(&self) -> String {
+        let (hours, left) = (*self / 3600, *self % 3600);
+        let (minutes, seconds) = (left / 60, left % 60);
         format!("{}:{:02}:{:02}", hours, minutes, seconds) // :02 cuts it to 2 chars
-    }
-}
-
-struct Timer {
-    last_instant: Instant,
-    delta: Duration,
-    period: Duration,
-    countdown: Duration,
-    ready: bool,
-}
-
-impl Timer {
-    fn new() -> Self {
-        let now = Instant::now();
-        Self {
-            last_instant: now,
-            delta: Duration::default(),
-            period: Duration::from_millis(1000),
-            countdown: Duration::default(),
-            ready: true,
-        }
-    }
-    fn update(&mut self){
-        let now = Instant::now();
-        self.delta = now - self.last_instant;
-        self.last_instant = now;
-        // Check Subtraction for result check
-        self.countdown = self.countdown.checked_sub(self.delta).unwrap_or_else(|| {
-            self.ready = true;
-            self.period
-        });
     }
 }
