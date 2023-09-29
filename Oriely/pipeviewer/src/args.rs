89ae421ff42d1080f913f857c9d1cb05af8c0301
skshm <eminate894567@protonmail.com>
2023-09-29 16:24:56 +0530

    modified for new clap!!

--- Oriely/pipeviewer/src/args.rs
+++ Oriely/pipeviewer/src/args.rs
@@ -1,5 +1,5 @@
-use clap::{App, Arg};
-use std::env;
+use clap::{Arg, Command};
+use std::{env};
 
 pub struct Args {
     pub infile: String,
@@ -9,25 +9,35 @@ pub struct Args {
 
 impl Args {
     pub fn parse() -> Self {
-        let matches = App::new("pipeviewer")
-            .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
+        let matches = Command::new("pipeviewer")
+            .arg(Arg::new("infile").help("Read from a file instead of stdin"))
             .arg(
-                Arg::with_name("outfile")
+                Arg::new("outfile")
                     .short('o')
                     .long("outfile")
-                    .takes_value(true)
+                    // .takes_value(true)
                     .help("Write output to file instead of stdout"),
             )
             .arg(
-                Arg::with_name("silent")
+                Arg::new("silent")
                     .short('s')
                     .long("silent")
                     .help("Silence the output"),
             )
             .get_matches(); // Get the matches from the command line
-        let infile = matches.value_of("infile").unwrap_or_default().to_string(); // Get the infile value from the matches
-        let outfile = matches.value_of("outfile").unwrap_or_default().to_string(); // Get the outfile value. unwrap_or_default sets default value to empty string
-        let silent = if matches.is_present("silent") {
+                            // Get the infile value from the matches
+        let infile = matches
+            .get_many::<String>("infile")
+            .unwrap_or_default()
+            .into_iter().map(|s| s.to_string())
+            .collect();
+        // Get the outfile value. unwrap_or_default sets default value to empty string
+        let outfile = matches
+            .get_many::<String>("outfile")
+            .unwrap_or_default()
+            .into_iter().map(|s| s.to_string())
+            .collect();
+        let silent = if matches.contains_id("silent") {
             true
         } else {
             !env::var("PV_SILENT").unwrap_or_default().is_empty()
